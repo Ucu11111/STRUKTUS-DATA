29b853880a59a9975c004622f2506f422fa65f34
Ucu11111 <yusufmalo44@gmail.com>
2025-06-04 12:52:46 +0800

    Update tugas2.py

--- struskturdata/PRE-PERULANGAN/tugas2.py
+++ struskturdata/PRE-PERULANGAN/tugas2.py
@@ -1,5 +1,12 @@
-batas = int(input("masukkan batas deret: "))
+batas = int(input("Masukkan batas deret: "))#Program minta kamu memasukkan angka terakhir dari deret yang ingin dibuat.
 
-for i in range(1, batas + 1, 2):
-    print(i, end=', ' if i + 2 <= batas else '\n')
-print(f"berikut hasil dari deret: {batas}")
+
+
+for i in range(1, batas + 1, 2):#Program mulai menghitung dari angka 1, lalu lompat 2 angka setiap kali sampai mencapai angka batas.
+
+
+    print(i, end=', ' if i + 2 <= batas else '\n')#Program menampilkan angka-angka deret yang sudah dihitung, dipisah dengan koma, kecuali angka terakhir yang langsung ganti baris baru.
+
+
+print(f"berikut hasil dari deret: {batas}")#Setelah selesai, program menampilkan pesan bahwa deret sampai angka yang kamu masukkan sudah selesai dibuat.
+
